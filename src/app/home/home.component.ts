b00cfb04784079291c9305328d71ae55d68a345f
Pierre Hebinger <hebinger.68@gmail.com>
2021-01-13 17:54:10 +0100

    Heroku Wake

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadXMLDoc();
+
+  }
+
+  loadXMLDoc(): void {
+    //reveiller le Back Heroku
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        console.log("HerokuBack UP");
+      }
+    };
+    xhttp.open("GET", "https://web-pierre-hebinger-back.herokuapp.com/", true);
+    xhttp.send();
   }
 
 }
